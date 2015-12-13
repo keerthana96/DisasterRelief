01c57fadf27add23ee82e8ac17586a161f9dd95f
nk-shruti <nks.shruti@gmail.com>
2015-12-13 10:50:41 +0530

    changes

--- database/create_volunteer_table.php
+++ database/create_volunteer_table.php
@@ -3,7 +3,9 @@
 $servername = "localhost";
 $username = "root";
 // $password = "root";
-$password = "9701c$%";
+
+$password = "root";
+
 $states = array("TamilNadu");
 $cities = array("TamilNadu"=>array("Chennai","Trichy"));
 $areas = array("Chennai"=>array("Poongavanapuram","two"),"Trichy"=>array("three","four"));
